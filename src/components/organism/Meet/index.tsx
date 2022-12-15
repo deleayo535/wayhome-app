f9e8f50895d4cfefa177823e5836a1169fd1f31b
Ayodele Afolabi <105075157+deleayo535@users.noreply.github.com>
2022-12-15 11:33:35 +0100

    Meet section

---
+++ src/components/organism/Meet/index.tsx
@@ -0,0 +1,46 @@
+import { Button } from "../../atoms/Button";
+import { Image } from "../../atoms/Image";
+import classes from "./meet.module.css";
+
+export function Meet() {
+  return (
+    <div className={`${classes.meet}`}>
+      <div className="flex justify-between space-x-4">
+        <div className="w-2/4 mt-10 ml-8">
+          <div className={`${classes.meetTxt}`}>
+            <div className={`${classes.meetH1}`}>Meet the Payne family</div>
+            <p className={`pb-6 ${classes.meetP1}`}>
+              Before we helped The Payne family buy their dream home, they were
+              struggling to save enough deposit...
+            </p>
+            <p className="text-lg pb-6">
+              “For anyone who has good credit, reasonable household income but
+              not a big enough deposit, this is the perfect route to getting on
+              the ladder..
+            </p>
+            <p className="text-lg pb-6">
+              It was important for us to own our home and have that sense of
+              security, and we treat our staircasing payments just as we would a
+              mortgage.”
+            </p>{" "}
+            <p className="text-lg underline">
+              Read more about the Payne family
+            </p>
+          </div>
+        </div>
+        <div className="w-2/4 mt-10 mr-8">
+          <Image
+            src="	https://www.wayhome.co.uk/static/media/payne-family.b95e626d.svg"
+            width="520px"
+            height=""
+          />
+        </div>
+      </div>
+      <div className="py-10">
+        <div className="flex ml-8">
+          <Button text="Start an application" />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -2,6 +2,7 @@ import { Hero } from "../../components/organism/Hero";
 import { NavBar } from "../../components/organism/NavBar";
 import { Section } from "../../components/molecules/Section";
 import { Works } from "../../components/organism/Works";
+import { Meet } from "../../components/organism/Meet";
 
 export default function LandingPage() {
   return (
@@ -9,6 +10,7 @@ export default function LandingPage() {
       <NavBar />
       <Hero />
       <Works />
+      <Meet />
     </div>
   );
 }
